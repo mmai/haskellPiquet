7a209589b0a23eea948e2e9d2282ae8534892e3e
Henri Bourcereau <henri.bourcereau@gmail.com>
2018-08-24 18:37:25 +0200

    socket with utf8

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,8 @@
 module Main where
-
  
 import Network (listenOn, withSocketsDo, accept, PortID(..), Socket)
 import System.Environment (getArgs)
-import System.IO (hSetBuffering, hGetLine, hPutStrLn, BufferMode(..), Handle)
+import System.IO (utf8, hSetBuffering, hSetEncoding, hGetLine, hPutStrLn, BufferMode(..), Handle)
 import Control.Concurrent (forkIO)
 
 import Game
@@ -19,8 +18,19 @@ main = withSocketsDo $ do
 sockHandler :: Socket -> IO ()
 sockHandler sock = do
     (handleP1, _, _) <- accept sock
-    hSetBuffering handleP1 NoBuffering
+    configureSocketHandler handleP1
+    -- hSetBuffering handleP1 NoBuffering
+    -- hSetEncoding handleP1 utf8
+    hPutStrLn handleP1 "New game. Waiting for your opponent to connect..."
     (handleP2, _, _) <- accept sock
-    hSetBuffering handleP2 NoBuffering
+    configureSocketHandler handleP2
+    -- hSetBuffering handleP2 NoBuffering
+    -- hSetEncoding handleP2 utf8
+    hPutStrLn handleP2 "Game starting..."
     forkIO $ play handleP1 handleP2
     sockHandler sock
+
+configureSocketHandler :: Handle -> IO ()
+configureSocketHandler handle = do
+    hSetBuffering handle NoBuffering
+    hSetEncoding handle utf8
